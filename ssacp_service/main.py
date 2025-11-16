92a1585ea449927c6b337567c81f9022519b5c75
Victor Franco <franco@Francos-MacBook-Air.local>
2025-11-16 13:21:06 -0300

    refactor: ssacp main for mongo urls

--- ssacp_service/main.py
+++ ssacp_service/main.py
@@ -5,11 +5,15 @@ import os
 
 app = FastAPI()
 
-MONGO_HOST = os.getenv("MONGO_HOST", "mongo")
-MONGO_PORT = int(os.getenv("MONGO_PORT", 27017))
-client = MongoClient(host=MONGO_HOST, port=MONGO_PORT)
-db = client["gp_db"]
-collection = db["tire_data"]
+MONGO_URI = "mongodb://mongo1:27017,mongo2:27017,mongo3:27017/?replicaSet=rs0"
+
+try:
+    client = MongoClient(MONGO_URI)
+    db = client["gp_db"]
+    collection = db["tire_data"]
+    print(f"[SSACP] Conectado ao Cluster MongoDB: {MONGO_URI}")
+except Exception as e:
+    print(f"[SSACP] Erro crítico ao conectar no Banco: {e}")
 
 class TireData(BaseModel):
     car_id: str
@@ -20,10 +24,8 @@ class TireData(BaseModel):
 def receive_tire_data(data: TireData):
     try:
         tire_dict = data.dict()
-        
         result = collection.insert_one(tire_dict)
-        
-        print(f"[SSACP] Dados recebidos e salvos! ID: {result.inserted_id}")
+        print(f"[SSACP] Dados salvos! ID: {result.inserted_id}")
         return {"status": "success", "db_id": str(result.inserted_id)}
     except Exception as e:
         print(f"[SSACP] Erro ao salvar: {e}")
